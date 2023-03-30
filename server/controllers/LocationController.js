6ac0872998eee44c278fec5652165be771152f58
Drake Scott <drakescott999@hotmail.com>
2023-03-30 14:37:51 -0400

    Event endpoints

---
+++ server/controllers/LocationController.js
@@ -0,0 +1,14 @@
+import { db } from "../db.js";
+import jwt from "jsonwebtoken";
+
+export const addLocation = (req, res) => {
+
+}
+
+export const getLocation = (req, res) => {
+
+}
+
+export const delLocation = (req, res) => {
+
+}
--- server/routes/events.js
+++ server/routes/events.js
@@ -1,9 +1,11 @@
 import Express from "express";
-import { getEvents } from "../controllers/EventController.js";
+import { getEvents, createEvent, deleteEvent } from "../controllers/EventController.js";
 
 const router = Express.Router();
 
-// router.get("/test", test)
+router.get("/", getEvents);
+router.post("/", createEvent);
+router.delete("/:id", deleteEvent);
 
 
 export default router;
